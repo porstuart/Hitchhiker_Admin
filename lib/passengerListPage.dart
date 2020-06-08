dee4de6d25f7266ae93b60b3bdf8cdbcc6fd1cbb
porstuart <stuart_por@soc.uum.edu.my>
2020-06-08 22:58:35 +0800

    Approval List Updated

--- lib/passengerListPage.dart
+++ lib/passengerListPage.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:hitchhiker_admin/passenger.dart';
-import 'package:toast/toast.dart';
+import 'package:hitchhiker_admin/slideRightRoute.dart';
 import 'package:http/http.dart' as http;
 import 'package:progress_dialog/progress_dialog.dart';
 
 double perpage = 1;
-String urlBlacklist =
-    "http://pickupandlaundry.com/hitchhiker/php/admin/blacklistPassenger.php";
 
 class PassengerList extends StatefulWidget {
   final Passenger passenger;
@@ -17,10 +15,10 @@ class PassengerList extends StatefulWidget {
   PassengerList({Key key, this.passenger});
 
   @override
-  _PassengerListPageState createState() => _PassengerListPageState();
+  _PassengerListState createState() => _PassengerListState();
 }
 
-class _PassengerListPageState extends State<PassengerList> {
+class _PassengerListState extends State<PassengerList> {
   GlobalKey<RefreshIndicatorState> refreshKey;
   List data;
 
@@ -51,24 +49,107 @@ class _PassengerListPageState extends State<PassengerList> {
                       return Container(
                         child: Column(
                           children: <Widget>[
+                            Stack(children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Center(
+                                    child: Text("Hitchhiker",
+                                        style: TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black)),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Container(
+                                    width: 300,
+                                    height: 140,
+                                    child: Card(
+                                      child: Padding(
+                                        padding: EdgeInsets.all(5.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceEvenly,
+                                          children: <Widget>[
+                                            Row(
+                                              children: <Widget>[
+                                                Icon(
+                                                  Icons.person,
+                                                ),
+                                                SizedBox(
+                                                  width: 5,
+                                                ),
+                                                Flexible(
+                                                  child: Text(
+                                                    widget.passenger.fName
+                                                                .toUpperCase() +
+                                                            " " +
+                                                            widget
+                                                                .passenger.lName
+                                                                .toUpperCase() ??
+                                                        "Not registered",
+                                                    style: TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: <Widget>[
+                                                Icon(
+                                                  Icons.phone_android,
+                                                ),
+                                                SizedBox(
+                                                  width: 5,
+                                                ),
+                                                Flexible(
+                                                  child: Text(widget
+                                                      .passenger.phoneNum),
+                                                ),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: <Widget>[
+                                                Icon(
+                                                  Icons.card_membership,
+                                                ),
+                                                SizedBox(
+                                                  width: 5,
+                                                ),
+                                                Flexible(
+                                                  child: Text(
+                                                      widget.passenger.matric),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ]),
+                            SizedBox(
+                              height: 4,
+                            ),
                             Container(
                               color: Colors.blue,
                               child: Center(
-                                child: Text("Passenger List",
+                                child: Text("Posted Trip",
                                     style: TextStyle(
                                         fontSize: 16,
                                         fontWeight: FontWeight.bold,
                                         color: Colors.white)),
                               ),
                             ),
-                            Container(
-                              child: Text("Click to blacklist account"),
-                            ),
                           ],
                         ),
                       );
                     }
-
                     if (index == data.length && perpage > 1) {
                       return Container(
                         width: 250,
@@ -88,17 +169,35 @@ class _PassengerListPageState extends State<PassengerList> {
                       child: Card(
                         elevation: 2,
                         child: InkWell(
-                          onTap: () => _onBlacklist(data[index]['email']),
+                          onTap: () => _onPassengerDetail(
+                              data[index]['email'],
+                              data[index]['fName'],
+                              data[index]['lName'],
+                              data[index]['matric'],
+                              data[index]['phoneNum'],
+                              data[index]['emergeNum'],
+                              data[index]['residentialHall']),
+                          onLongPress: _onJobDelete,
                           child: Padding(
                             padding: const EdgeInsets.all(2.0),
                             child: Row(
                               children: <Widget>[
+                                Container(
+                                    height: 100,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(color: Colors.white),
+                                        image: DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: AssetImage(
+                                                "assets/images/trip.jpg")))),
                                 Expanded(
                                   child: Container(
                                     child: Column(
                                       children: <Widget>[
                                         Text(
-                                            data[index]['email']
+                                            data[index]['destination']
                                                 .toString()
                                                 .toUpperCase(),
                                             style: TextStyle(
@@ -107,6 +206,15 @@ class _PassengerListPageState extends State<PassengerList> {
                                         SizedBox(
                                           height: 5,
                                         ),
+                                        Text(data[index]['origin']),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(data[index]['depatureDate']),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(data[index]['depatureTime']),
                                       ],
                                     ),
                                   ),
@@ -122,15 +230,18 @@ class _PassengerListPageState extends State<PassengerList> {
   }
 
   Future<String> makeRequest() async {
-    String urlLoadPassenger =
-        "http://pickupandlaundry.com/hitchhiker/php/admin/loadPassenger.php";
+    String urlLoadTrip =
+        "http://pickupandlaundry.com/hitchhiker/php/loadPostedTrip.php";
     ProgressDialog pr = new ProgressDialog(context,
         type: ProgressDialogType.Normal, isDismissible: false);
-    pr.style(message: "Loading Passenger");
-    http.post(urlLoadPassenger, body: {}).then((res) {
+    pr.style(message: "Loading Trip");
+    pr.show();
+    http.post(urlLoadTrip, body: {
+      "email": widget.passenger.email ?? "notavail",
+    }).then((res) {
       setState(() {
         var extractdata = json.decode(res.body);
-        data = extractdata["passenger"];
+        data = extractdata["trips"];
         perpage = (data.length / 10);
         print("data");
         print(data);
@@ -153,54 +264,26 @@ class _PassengerListPageState extends State<PassengerList> {
     return null;
   }
 
-  void _onBlacklist(String email) {
-    Passenger passenger = new Passenger(email: email);
-    print(email);
-    if (email == "not register") {
-      Toast.show("Not allowed", context,
-          duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
-      return;
-    }
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: new Text("Blacklist " + email),
-          content: new Text("Are you sure?"),
-          actions: <Widget>[
-            new FlatButton(
-              child: new Text("Yes"),
-              onPressed: () {
-                http.post(urlBlacklist, body: {
-                  "email": email,
-                }).then((res) {
-                  var string = res.body;
-                  List dres = string.split(",");
-                  if (dres[0] == "success") {
-                    print('in success');
-                    setState(() {
-                      email = dres[1];
-                      if (dres[0] == "success") {
-                        Toast.show("Success", context,
-                            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
-                        Navigator.of(context).pop();
-                      }
-                    });
-                  } else {}
-                }).catchError((err) {
-                  print(err);
-                });
-              },
-            ),
-            new FlatButton(
-              child: new Text("No"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+  void _onPassengerDetail(
+      String email,
+      String fName,
+      String lName,
+      String matric,
+      String phoneNum,
+      String emergeNum,
+      String residentialHall) {
+    Passenger passenger = new Passenger(
+      email: email,
+      fName: fName,
+      lName: lName,
+      matric: matric,
+      phoneNum: phoneNum,
+      emergeNum: emergeNum,
+      residentialHall: residentialHall,
     );
+  }
+
+  void _onJobDelete() {
+    print("Delete");
   }
 }
